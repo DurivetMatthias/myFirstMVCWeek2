1de6da5a143d26414a5791c263547d95ac617536
matthiasdurivet <Matthias.durivet@student.howest.be>
2018-02-19 11:29:01 +0100

    index function

---
+++ myFirstMVCWeek2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace myFirstMVCWeek2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string word)
+        {
+            return $"cute {word}";
+        }
+    }
+}
